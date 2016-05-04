9c1ee578d3445575f9cf3069493302a3368ac0f9
TrAnDuNg <t.dung90@gmail.com>
2016-05-04 15:50:59 +0700

    fix

--- gen_passwd.py
+++ gen_passwd.py
@@ -4,17 +4,17 @@ import random
 PASSWD_LENGTH = 16
 
 
-def gen_passwd2():
+def gen_passwd():
     result = ""
-    all_char = [
+    all_chars = [
         string.ascii_lowercase, string.ascii_uppercase,
         string.digits, string.punctuation]
-    for s in all_char:
+    for s in all_chars:
         result += random.choice(s)
-    result += ("".join(random.sample("".join(all_char), PASSWD_LENGTH - len(result))))
+    result += ("".join(random.sample("".join(all_chars), PASSWD_LENGTH - len(result))))
     return "".join(random.sample(result, len(result)))
 
 
 if __name__ == "__main__":
 
-    print gen_passwd2()
+    print gen_passwd()
